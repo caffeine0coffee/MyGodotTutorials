65a295effbac4800d541ce24be2ea1470e42ebfd
caffeine <caffeine@caffeine-LinuxMint21.2>
2023-11-25 01:11:01 +0900

    first

---
+++ Tutorial02_MyFirstScript/Sprite2D.gd
@@ -0,0 +1,38 @@
+extends Sprite2D
+
+var angular_speed = PI
+var speed = 400
+
+func _init():
+	print("Hello, World!!")
+
+
+func _ready():
+	var timer = get_node("Timer")
+	timer.timeout.connect(_on_timer_timeout)
+
+
+func _process(delta):
+	var direction = 0
+
+	if Input.is_action_pressed("ui_left"):
+		direction = -1;
+	if Input.is_action_pressed("ui_right"):
+		direction = 1;
+
+	rotation += angular_speed * direction * delta
+
+	var velocity = Vector2.ZERO
+
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+
+	position += velocity * delta
+
+
+func _on_button_pressed():
+	set_process(not is_processing())
+
+
+func _on_timer_timeout():
+	visible = not visible
